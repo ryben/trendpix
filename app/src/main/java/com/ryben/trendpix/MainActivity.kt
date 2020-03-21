0ce5ce2558d944175a9135add1fdb2e12fea2779
Rey Benedicto <reybenedicto06@gmail.com>
2020-03-21 13:48:43 +0800

    Setup NavHostFragment; Show HomeFragment

--- app/src/main/java/com/ryben/trendpix/MainActivity.kt
+++ app/src/main/java/com/ryben/trendpix/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.ryben.trendpix
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ryben.trendpix.home.view.HomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
